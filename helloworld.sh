693926e766736d0b50ad9f2b955e7b9f292d85a1
mikes <mikes@cyberrange>
2023-10-24 12:46:52 -0700

    added hellowworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Michael Sineiro
+# Date of latest revision:      10/24/2023
+# Purpose:                      Print a string
+
+# Declaration of variables
+var_a="HelloWorld"
+var_b="I am working perfectly"
+
+# Declaration of functions
+
+
+# Main
+echo $var_a
+sleep 3
+echo $var_b
+
+
+# End
