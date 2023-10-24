eda13deef27222c77e34b7632ff2e0e868f19e46
mikes <mikes@cyberrange>
2023-10-24 11:56:35 -0700

    added notes

---
+++ example.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Script: Name of the Script?
+# Author: Who is the Author
+# Date: Date or Revisions?
+# Purpose: What does it do?
+# How: How do you execute it?
+# Variables: Main Variables used
+# Libraries: What libraries used?
+# Externals Sources: 
+
+# @ Ask the user their age
+
+# User: How old are you. The user says, I'm 49. You store this in a variable.
+# called user_age
+
+# user_ag = 49
+# print(user_age) -> 49
+
+echo "hi"
