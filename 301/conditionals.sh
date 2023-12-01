a1dba15f46db962d9c076e6ff84e92eaa8044e80
mikes <mikes@cyberrange>
2023-11-30 22:18:46 -0800

    terrible

--- 301/conditionals.sh
+++ 301/conditionals.sh
@@ -5,20 +5,21 @@
 # Date of latest revision:      11/30/2023
 # Purpose:                      Creates a menu and executes some basic commands.
 # Execution:                    bash conditionals.sh ./conditionals.sh chmod -x conditionals.sh
-
+# 
 # Decleration of variables
 
 # Decleration of functions
+# what a day
 function hello {
     echo "Hello world!"
     read -n1 -r -p "Press any key to continue..."
 }
-
+# still pissed about the lab
 function loopback {
     ping -c 1 127.0.0.1
     read -n1 -r -p "Press any key to continue..."
 }
-
+# thank god we spent an hour going over code challenges today
 function ip {
     ifconfig
     read -n1 -r -p "Press any key to continue..."
@@ -26,7 +27,7 @@ function ip {
 
 # Main
 
-
+# its a while loop
 while true; do
     clear
     echo "Menu:"
@@ -35,7 +36,7 @@ while true; do
     echo "3. IP info"
     echo "4. Exit"
     read -p "Enter your choice: " choice
-
+# case and it calls funcations or something
     case $choice in
         1)
             hello
@@ -57,5 +58,5 @@ while true; do
     esac
 done
 
-
+# explain the lab better and i'll explain the code better.
 # End
