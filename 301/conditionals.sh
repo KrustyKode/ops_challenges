230efe6cf740084216e34669cdd74b48ab8f4167
mikes <mikes@cyberrange>
2023-12-01 11:18:07 -0800

    terrible

--- 301/conditionals.sh
+++ 301/conditionals.sh
@@ -19,7 +19,7 @@ function loopback {
     ping -c 1 127.0.0.1
     read -n1 -r -p "Press any key to continue..."
 }
-# thank god we spent an hour going over code challenges today
+
 function ip {
     ifconfig
     read -n1 -r -p "Press any key to continue..."
